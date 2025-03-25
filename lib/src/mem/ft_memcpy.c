3e805c528f5c7694117748ceb2e1ddef1d1e1b17
jd17jd <juandieg01888@gmail.com>
2025-03-25 14:39:05 +0800

    back

--- lib/src/mem/ft_memcpy.c
+++ lib/src/mem/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jvivas-g <jvivas-g@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 14:13:19 by jvivas-g          #+#    #+#             */
-/*   Updated: 2025/03/25 14:34:13 by jvivas-g         ###   ########.fr       */
+/*   Updated: 2025/03/25 14:37:35 by jvivas-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
-	if (!src || !dst)
-		return (NULL);
 	i = 0;
+	if (src == NULL && dst == NULL)
+		return (NULL);
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
